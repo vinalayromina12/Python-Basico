911b422caa2d1ee6db1307525086dec4ae96fe16
vinalayromina12-rc <vinalayromina12@gmail.com>
2026-03-02 15:45:29 -0800

    actualizacion

--- python.basico/ogro.py
+++ python.basico/ogro.py
@@ -1,11 +1,17 @@
 from Enemigo import *
+import random
 
 class Ogro(Enemigo):
 
     def __init__(self, puntos_energia=20, ataque=3):
-        super().__init__(tipo_enemigo='Ogro',
-                         puntos_energia=puntos_energia,
-                         ataque=ataque)
-
-    def habla(self):
-        print("¡Ogro aplastar todo!!!")
+        super().__init__(tipo_enemigo='Ogro',puntos_energia=puntos_energia,ataque=ataque)
+        
+        def habla(self):
+         print("Ogro aplastar todo!!!")
+        
+        def ataque_especial(self):
+         print("Ogro ataque especial")
+         funciona_ataque_especial = random.random() < 0.20
+         if funciona_ataque_especial:
+             self.ataque += 4
+             print("Ogro enojado y incremento su ataque por 4")
