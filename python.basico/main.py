506f0f8253d20e89aabe3c736e0def6cb2f0b830
vinalayromina12-rc <vinalayromina12@gmail.com>
2026-03-02 17:39:47 -0800

    Correccion

--- python.basico/main.py
+++ python.basico/main.py
@@ -2,31 +2,35 @@ from Enemigo import *
 from zombie import *
 from ogro import *
 
-zombie =Zombie(10,1)
-ogro = Ogro(20,3)
-def batalla (e1: Enemigo, e2: Enemigo):
+
+zombie = Zombie(10, 1)
+ogro = Ogro(20, 3)
+
+def batalla(e1: Enemigo, e2: Enemigo):
     e1.habla()
     e2.habla()
 
-    while e1.punto_energia > 0 and e2.punto_energia >0:
-        print("#############")
+    while e1.puntos_energia > 0 and e2.puntos_energia > 0:
+        print("###################")
         e1.ataque_especial()
         e2.ataque_especial()
-        print(f"{e1.get_tipo_enemigo()}:quedan : {e1.punto_energia}puntos de energia")
-        print(f"{e2.get_tipo_enemigo()}:quedan : {e2.punto_energia}puntos de energia")
-        print(f"Ataque: { e2.ataque}")
-        e1.punto_energia -= e2.ataque
-        print("====================")
-        print(f"Ataque:{e1.ataque}")
-        e2.punto_energia -= e1.ataque
+        print(f"{e1.get_tipo_enemigo()}: quedan: {e1.puntos_energia} puntos de energia")
+        print(f"{e2.get_tipo_enemigo()}: quedan: {e2.puntos_energia} puntos de energia")
+        print(f"Ataque: {e2.ataque}")
+        e1.puntos_energia -= e2.ataque
+        print("===============")
+        print(f"Ataque: {e2.ataque}")
+        e2.puntos_energia -= e1.ataque
 
-    print("###################")
-    if e1.punto_energia > 0:
-           print(f"{e1.get_tipo_enemigo()}gano!!")
-    else:
-        print(f"{e2.get_tipo_enemigo()}gano!!")
-print("============BATALLA===========")
-batalla(zombie,ogro)              
-#print(f"{zombie.get_tipo_enemigo()}tiene{zombie.punto_energia}de energia y ataca con {zombie.ataque}")
-#print(f"{zombie.get_tipo_enemigo()}tiene{ogro.punto_energia}de energia y ataca con {ogro.ataque}")
+    print("####################")
+    if e1.puntos_energia > 0:
+        print(f"{e1.get_tipo_enemigo()} gano!!!")
+    else: 
+        print(f"{e2.get_tipo_enemigo()} gano!!!!!!!!!")
 
+
+print("================BATALLA================")
+batalla(zombie, ogro)
+print("================FIN DE LA BATALLA===============")
+print(f"{zombie.get_tipo_enemigo()} tiene {zombie.puntos_energia} de energia y ataca con {zombie.ataque}")
+print(f"{ogro.get_tipo_enemigo()} tiene {ogro.puntos_energia} de energia y ataca con {ogro.ataque}")
